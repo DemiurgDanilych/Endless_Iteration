7d2e3970e01c365f4f2d696ff50ecdfab859c12c
Danil Agafonov <javaDanilAgafonov@yandex.ru>
2021-08-04 06:45:51 +0300

    Bug fix: Fixed the constructor in the Randoms class

--- Main.java
+++ Main.java
@@ -1,17 +1,11 @@
-import java.util.Iterator;
 
 public class Main {
 	public static void main(String[] args) {
-		Randoms randoms = new Randoms(90, 100);
-		Iterator iterator = randoms.iterator();
-		
-		while (iterator.hasNext()) {
-			int i = (int) iterator.next();
-			if (i == 100) {
+		for (int r : new Randoms(90, 100)) {
+			System.out.println("Случайное число: " + r);
+			if (r == 100) {
 				System.out.println("Выпало число 100, давайте на этом закончим");
 				break;
-			} else {
-				System.out.println("Случайное число: " + i);
 			}
 		}
 	}
--- Randoms.java
+++ Randoms.java
@@ -3,17 +3,17 @@ import java.util.*;
 
 public class Randoms implements Iterable<Integer> {
 	protected Random random = new Random();
-	protected List<Integer> randomNumber;
+	protected List<Integer> randomNumber = new ArrayList<>();
 	
 	public Randoms(int min, int max) {
 		
-		List<Integer> randomNumberMethod = new ArrayList<>();
-		
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; ; i++) {
 			int r = random.nextInt(max - min) + (min +1);
-			randomNumberMethod.add(r);
+			randomNumber.add(r);
+			if (randomNumber.get(i) == 100){
+				break;
+			}
 		}
-		this.randomNumber = randomNumberMethod;
 	}
 	
 	@Override
