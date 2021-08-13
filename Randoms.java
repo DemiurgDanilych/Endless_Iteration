3d70da2936f444bd333d41ebf8954960b2c75170
Danil Agafonov <javaDanilAgafonov@yandex.ru>
2021-08-13 06:36:37 +0300

    Refactor class Random

--- Randoms.java
+++ Randoms.java
@@ -2,34 +2,30 @@ import java.util.*;
 
 
 public class Randoms implements Iterable<Integer> {
-	protected Random random = new Random();
-	protected List<Integer> randomNumber = new ArrayList<>();
+	
+	protected Random random;
+	private int min;
+	private int max;
 	
 	public Randoms(int min, int max) {
-		
-		for (int i = 0; ; i++) {
-			int r = random.nextInt(max - min) + (min +1);
-			randomNumber.add(r);
-			if (randomNumber.get(i) == 100){
-				break;
-			}
-		}
+		random = new Random();
+		this.min = min;
+		this.max = max;
 	}
 	
 	@Override
 	public Iterator<Integer> iterator() {
 		
 		return new Iterator() {
-			int next = 0;
 			
 			@Override
 			public boolean hasNext() {
-				return next < randomNumber.size();
+				return true;
 			}
 			
 			@Override
 			public Integer next() {
-				return randomNumber.get(next++);
+				return random.nextInt(max - min) + (min + 1);
 			}
 		};
 	}
